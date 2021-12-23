5b56372b530870af86b5d299a29f98dfef0a6012
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-12-22 19:01:29 -0800

    draft swich statement in .thn index

--- index.js
+++ index.js
@@ -7,46 +7,90 @@
 const dbConnect = require('./db/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const viewOptions = require('./lib/viewQueries');
+const { viewDepartments, viewRoles, viewEmployees } = require('./lib/viewQueries');
+// const viewOptions = require('./lib/viewQueries');
+// const viewDepartments = require('./lib/viewQueries');
 
 
 
-dbConnect.connect(function (err) {
-  if (err) throw err;
-  
 
 const optionPrompt = () => {
-
-  console.log (`
+  console.log(`
   
-  ============== What do you want to do? =============
+  ============== Welcome to Employee Tracker! =============
   
   `);
-
-
   return inquirer
     .prompt([
       // ****** Option Menu ******
       {
         type: 'list',
         name: 'option',
-        message: 'Pick ONE license.',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee Role', 'Exit']
+        message: 'What do you want to do?',
+        choices:
+          ['View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee Role',
+            'Exit']
       }
     ])
-    .then(choice => {
-      console.log(choice)
-      return choice;
-    })
-  };
+    .then((answer) => {
+      console.log(answer)
+      // answer.option  or destructure and pull option out  they are the same things
+      switch (answer.option) {
 
-// ** Running the app
-optionPrompt()
-.then(answer => {
-  return viewOptions(answer)
-})
-  .catch(err => {
-    console.log(err);
-  });
+        case 'View all departments':
+          viewDepartments();
+          break;
 
-})
+        case 'View all roles':
+          viewRoles();
+          break;
+
+        case 'View all employees':
+          viewEmployees();
+          break;
+
+        default:
+          dbConnect.end(function (err) {
+            console.log('Thank you! Goodbye.')
+          })
+        }
+      })
+
+    }
+            // const { action } = answer
+
+
+            //  if (action === 'View all departments') {
+
+            // viewDepartments();
+
+            //  }
+            // if (action === 'Exit') {
+
+            //   dbConnect.end(function (err) {
+            //     console.log('Thank you! Goodbye.')
+            //   });
+            // }
+ 
+        // viewRoles();
+        // viewEmployees();
+    
+
+
+
+      // ** Running the app
+      // optionPrompt()
+      //   .then(choices => {
+      //     return (choices)
+      //   })
+      //   .catch(err => {
+      //     console.log(err);
+      //   });
+
+      module.exports = optionPrompt
