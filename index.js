fabfce90467b3233646efcc2c54c3d6db6553411
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-01-02 16:56:39 -0800

    try variations on repeat prompt placement

--- index.js
+++ index.js
@@ -3,98 +3,115 @@ const dbConnect = require('./db/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
+const util = require('util')
 const { viewDepartments, viewRoles, viewEmployees, viewOneEmp } = require('./lib/viewQueries');
 const { addDeptPrompt, addRolePrompt, addEmpPrompt } = require('./lib/addQueries')
-const { updateEmpPrompt} = require('./lib/updateQueries')
+const { updateEmpPrompt } = require('./lib/updateQueries')
 //const { deleteDept } = require('./lib/deleteQueries');
 
 
-optionPrompt = () => {
+
+//  ************  REPEAT MENU Function  *********
+repeatPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirmAgain',
+      message: 'Would you like to do something else?',
+      default: false,
+    },
+  ])
+    .then(data => {
+
+      // determine whether or not another action is wantes
+      if (data.confirmAgain) {
+        optionPrompt()
+      } else {
+        // dbConnect.end(function (err) {
+          console.log('Thank you! Goodbye.')
+        // })
+      }
+    });
+}
+
+
+//  **************  MAIN OPTION MENU   ***********
+const optionPrompt = () => {
   console.log(`
   
   ============== Your Employee Tracker =============
   
   `);
-inquirer.prompt(  
-  // ****** Option Menu ******
-      {
-        type: 'list',
-        name: 'option',
-        message: 'What do you want to do?',
-        choices:
-          ['View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Exit']
-      }
-    )
+  return inquirer.prompt(
+    // ****** Option Menu ******
+    {
+      type: 'list',
+      name: 'option',
+      message: 'What do you want to do?',
+      choices:
+        ['View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit']
+    }
+  )
     .then(answer => {
-      console.log(answer)
+      // console.log(answer)
       // answer.option  or destructure and pull option out  they are the same things
       switch (answer.option) {
+
         case 'View all departments':
-          console.log('departments')
-          viewDepartments();  
-          optionPrompt();
+          // console.log('departments')
+          viewDepartments();
+          repeatPrompt();
+          // repeatPrompt()
+          // .then(repeatPrompt)
+          // .catch(err => {
+          //   console.log(err);
+          // });
           break;
-          
-          // setTimeout(function() {
-          //   optionPrompt()
-          // }, 2000);
-          break;
+
+        // setTimeout(function() {
+        //   optionPrompt()
+        // }, 2000);
+
 
         case 'View all roles':
-          console.log('roles')
+          // console.log('roles')
           viewRoles();
-
-          setTimeout(function() {
-            optionPrompt()
-          }, 2000);
+          repeatPrompt();
           break;
 
         case 'View all employees':
           viewEmployees();
-          
-          setTimeout(function() {
-            optionPrompt()
-          }, 2000);
+          repeatPrompt();
           break;
 
-       
         case 'Add a department':
           addDeptPrompt();
-          // optionPrompt();
-          setTimeout(function() {
-            optionPrompt()
-          }, 1500);
+          // repeatPrompt();
           break;
 
-          
+
         case 'Add a role':
           addRolePrompt();
-          // optionPrompt();
-          // setTimeout(function() {
-          //   optionPrompt()
-          // }, 1500);
+          // repeatPrompt();
           break;
 
 
         case 'Add an employee':
           addEmpPrompt();
-          // optionPrompt();
-          // setTimeout(function() {
-          //   optionPrompt()
-          // }, 1500);
+          // repeatPrompt();
           break;
 
         case 'Update an employee role':
           updateEmpPrompt();
+          // repeatPrompt();
           break;
-
 
 
         // case 'Delete a department':
@@ -105,36 +122,52 @@ inquirer.prompt(
         //   }, 1500);
         //   break;
 
-
-      // case 'Exit':
-      default:
+        case 'Exit':
           dbConnect.end(function (err) {
             console.log('Thank you! Goodbye.')
           })
           break;
 
-      // default: 
-      // optionPrompt();
-      // break;
+        default:
+          dbConnect.end(function (err) {
+            console.log('Thank you! Goodbye.')
+          })
+          break;
 
-        } 
+        // default: 
+        // optionPrompt();
+        // break;
 
-      })
-      // .then(optionPrompt)
+      }
 
-    }
+    })
+  // .then(data => {
+  //   return repeatPrompt(data)
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  //   });
+}
 
 
-// module.exports = {
-//   optionPrompt
-// }
+//  ** Running the app
+optionPrompt()
+  .then(repeatPrompt())
+  // .then(data => {
+
+  // })
+  .catch(err => {
+    console.log(err);
+  });
 
 
- //  ** Running the app
-   optionPrompt()
-    // .then(choices => { 
-    // return(choices)
-    //    })
-    //    .catch(err => {
-    //      console.log(err);
-    //    });
+
+// module.exports = repeatPrompt()
+
+
+
+//  ******** DRAFT TIMEOUT   *******
+
+  // setTimeout(function() {
+  //   optionPrompt()
+  // }, 1500);
